4cf19ba0959128e7268e146ac96606a8b4e481b4
Kedar Damkondwar <39631662+KedarD1@users.noreply.github.com>
2023-04-26 06:20:01 +0000

    commit

--- Ex_Files/04_02_end/main.py
+++ Ex_Files/04_02_end/main.py
@@ -1,10 +1,13 @@
+from matplotlib import pyplot as plt
 from distutils.log import debug
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-  return "Hello, World!"
+    return "Hello, World"
+
 
 app.run(debug=True)
